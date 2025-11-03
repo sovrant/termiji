f7f295e53f8aa12e661999f9a6fef165f77df4bb
Sovr <234765it@gmail.com>
2025-11-03 12:34:18 +0700

    Json fetching(Emojis)
    I think the emoji.rs is finished now.
    Got the json fetching working and it could handle unlimited categories
    Mostly done by using hashmap, god truly created the hash.
    What a blessing!
    Will have to figure out how to make the json file avaliable for when users truly use this tool though.

---
+++ src/emoji.rs
@@ -0,0 +1,40 @@
+use std::{fs::File, io::Read};
+use std::collections::HashMap;
+use serde::Deserialize;
+
+#[derive(Deserialize, Clone, Debug, Hash, Eq, PartialEq)]
+pub struct Emoji {
+    emoji: String,
+    slug: String,
+    group: String,
+}
+
+impl Emoji {
+    pub fn get_emoji(&self) -> &str {
+        &self.emoji
+    }
+
+    pub fn get_slug(&self) -> &str {
+        &self.slug
+    }
+
+    pub fn get_group(&self) -> &str {
+        &self.group
+    }
+}
+
+pub fn extract_emoji(mut path: File) -> HashMap<String, Vec<Emoji>> {
+    let mut data = String::new();
+    path.read_to_string(&mut data).unwrap();
+    let emojis: Vec<Emoji> = serde_json::from_str(&data).unwrap();
+
+    let mut emoji_categories = HashMap::new();
+
+    for emoji in emojis {
+        emoji_categories.entry(emoji.group.clone())
+            .or_insert_with(Vec::new)
+            .push(emoji.clone());
+    }
+
+    emoji_categories
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod emoji;
+use std::fs::File;
+use simple_home_dir::*;
+use std::io::ErrorKind;
+
 fn main() {
-    println!("Hello, world!");
+    let home_dir = home_dir().unwrap();
+    let home_dir = home_dir.to_str();
+
+    //reading in the emojis
+    let path_emoji = format!(
+        "{}/.cache/termiji/data-by-emoji-category.json",
+        home_dir.unwrap()
+    );
+
+    let path = match File::open(&path_emoji) {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => panic!("Please, make sure the emoji json file exists at {}.", path_emoji),
+            _ => panic!("Error, trying to access emoji json file.")
+        }
+    };
+    
+    let emojis_hash = emoji::extract_emoji(path);
+
+    // for (category, emojis) in emojis_hash {
+    //     println!("Category: {}", category);
+    //
+    //     for emoji in emojis {
+    //         println!("Emoji: {}, {}, {}", emoji.get_emoji(), emoji.get_slug(), emoji.get_group());
+    //     }
+    // }
 }
